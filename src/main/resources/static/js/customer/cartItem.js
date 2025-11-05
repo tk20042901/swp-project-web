d92e013a7bf2f3061a37cbf001f318a837d2803d
tk20042901 <tk20042901@gmail.com>
2025-11-05 21:23:23 +0700

    fix domain name

--- src/main/resources/static/js/customer/cartItem.js
+++ src/main/resources/static/js/customer/cartItem.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://ntbinh.online/customer/cartItemNumber')
+    fetch('https://fruitshop.tech/customer/cartItemNumber')
         .then(response => {
             if (!response.ok) {
                 throw new Error('HTTP error status: ' + response.status);
